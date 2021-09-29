83dcedbfe062fc499b92198051b6b768c83a8709
Destin Lee <dleeinc@gmail.com>
2021-09-29 17:37:51 -0500

    new build

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
+/**
+ *
+ * @param length number - length of digits to add
+ * @returns string of char you can use for ids
+ */
 function id(length = 1000000) {
   return Date.now() + '-' + Math.floor(Math.random() * length);
 }
 
+/**
+ *
+ * @param log any - mapping for console.log
+ */
 function cl(log: any) {
   console.log(log);
 }
